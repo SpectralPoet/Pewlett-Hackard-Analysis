f09e4ce116647dad44a8001972b642834c461151
Spectral Poet <mitchghendrix@gmail.com>
2021-07-17 22:29:48 -0500

    complete thru 7.3:

--- schema.sql
+++ schema.sql
@@ -40,7 +40,7 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_emp (
@@ -49,7 +49,8 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+DROP TABLE dept_emp;
 SELECT * FROM departments;
